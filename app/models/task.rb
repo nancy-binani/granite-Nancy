ea492610c985ac96d5c17da2b60c8031cff34e95
Nancy Binani <nancybinani1207@gmail.com>
2022-09-07 12:41:39 +0530

    Added unit tests for task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,16 +2,18 @@
 
 class Task < ApplicationRecord
   RESTRICTED_ATTRIBUTES = %i[title task_owner_id assigned_user_id]
+
   enum progress: { pending: "pending", completed: "completed" }
   enum status: { unstarred: "unstarred", starred: "starred" }
-  has_many :comments, dependent: :destroy
-  belongs_to :task_owner, foreign_key: "task_owner_id", class_name: "User"
-  belongs_to :assigned_user, foreign_key: "assigned_user_id", class_name: "User"
-  MAX_TITLE_LENGTH = 125
 
+  has_many :comments, dependent: :destroy
+  belongs_to :assigned_user, foreign_key: "assigned_user_id", class_name: "User"
+  belongs_to :task_owner, foreign_key: "task_owner_id", class_name: "User"
+  MAX_TITLE_LENGTH = 125
   validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
   validates :slug, uniqueness: true
   validate :slug_not_changed
+
   before_create :set_slug
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
   MAX_EMAIL_LENGTH = 255
-  MAX_NAME_LENGTH = 35
+
   has_many :comments, dependent: :destroy
-  has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
+  has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
   has_secure_password
   has_secure_token :authentication_token
+  before_destroy :assign_tasks_to_task_owners
 
-  validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
+  validates :name, presence: true, length: { maximum: 35 }
   validates :email, presence: true,
     uniqueness: { case_sensitive: false },
     length: { maximum: MAX_EMAIL_LENGTH },
@@ -19,7 +20,6 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, on: :create
 
   before_save :to_lowercase
-  before_destroy :assign_tasks_to_task_owners
 
   private
 

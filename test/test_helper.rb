45da6979aab9d014add7b696fcfa86469f5f2a3d
Nancy Binani <nancybinani1207@gmail.com>
2022-09-07 15:14:27 +0530

    Added unit testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,21 +16,28 @@ end
 
 enable_test_coverage if ENV["COVERAGE"]
 
-# Previous content of test helper as is
-
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
   include ActionView::Helpers::TranslationHelper
   include FactoryBot::Syntax::Methods
-
+  # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors) unless ENV["COVERAGE"]
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  # fixtures :all
+  fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  # previous code
+
+  def headers(user, options = {})
+    {
+      Accept: "application/json",
+      "Content_Type" => "application/json",
+      "X-Auth-Token" => user.authentication_token,
+      "X-Auth-Email" => user.email
+    }.merge(options)
+  end
 end
